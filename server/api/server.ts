3586d7d6ea19a06690b8dd82837fc3964f1705be
Maky <franklinjaradev@gmail.com>
2022-01-12 12:02:26 -0500

    clean up: removing and adding files/dependencies

--- server/api/server.ts
+++
@@ -1,6 +0,0 @@
-// api/server.ts
-import { ApolloServer } from "apollo-server";
-import { context } from "./context";
-import { schema } from "./schema";
-
-export const server = new ApolloServer({ schema, context });
