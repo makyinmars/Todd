cc8741aa1e097cdcb9781a86a53a6e22c3feae41
Franklin Jara <franklinjaradev@gmail.com>
2022-07-10 20:40:36 -0400

    :necktie: T3 app
    Adding t3 app

--- client/src/lib/apollo.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: `https://todd-server.herokuapp.com/api`,
-  // uri: "http://localhost:4000/api",
-  cache: new InMemoryCache(),
-});
--- server/Procfile
+++
@@ -1,2 +0,0 @@
-web: npm start
-release: npx prisma migrate deploy
