9dff0a409d962a1165c0d00a14c84590911fdb84
Franklin Jara <franklinjaradev@gmail.com>
2022-07-11 10:51:55 -0400

    :sparkles: Crud for idea router
    Adding create, getIdeas and updateIdea

--- src/server/router/idea.ts
+++ src/server/router/idea.ts
@@ -4,29 +4,63 @@ import { Prisma } from "@prisma/client";
 import { prisma } from "@/server/db/client";
 import { TRPCError } from "@trpc/server/dist/declarations/src";
 
-// const defaultIdeaSelect = Prisma.validator<Prisma.IdeaSelect>()({
-//   id: true,
-//   title: true,
-//   content: true,
-//   imageUrl: true,
-//   votes: true,
-//   createdAt: true,
-//   updatedAt: true,
-// });
-
-export const ideaRouter = createRouter().query("getIdeas", {
-  async resolve() {
-    // const ideas = await prisma.idea.findMany({
-    //   select: defaultIdeaSelect,
-    // });
-    // if (ideas) {
-    //   return ideas;
-    // } else {
-    //   throw new TRPCError({
-    //     code: "BAD_REQUEST",
-    //     message: `Could not get trips`,
-    //   });
-    // }
-    return null
-  },
+const defaultIdeaSelect = Prisma.validator<Prisma.IdeaSelect>()({
+  id: true,
+  title: true,
+  content: true,
+  imageUrl: true,
+  votes: true,
+  createdAt: true,
+  updatedAt: true,
 });
+
+export const ideaRouter = createRouter()
+  .query("getIdeas", {
+    async resolve() {
+      const ideas = await prisma.idea.findMany({
+        select: defaultIdeaSelect,
+      });
+      if (ideas) {
+        return ideas;
+      } else {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Could not get trips`,
+        });
+      }
+    },
+  })
+  .mutation("createIdea", {
+    input: z.object({
+      title: z.string(),
+      content: z.string(),
+      imageUrl: z.string(),
+      votes: z.number(),
+    }),
+    async resolve({ input }) {
+      const idea = await prisma.idea.create({
+        data: input,
+        select: defaultIdeaSelect,
+      });
+
+      return idea;
+    },
+  })
+  .mutation("addVote", {
+    input: z.object({
+      id: z.string(),
+      votes: z.number(),
+    }),
+    async resolve({ input }) {
+      const ideaUpdated = await prisma.idea.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          votes: input.votes,
+        },
+      });
+
+      return ideaUpdated;
+    },
+  });
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,9 +1,12 @@
 // src/server/router/index.ts
-import { createRouter } from "./context";
 import superjson from "superjson";
+
+import { createRouter } from "./context";
+import { ideaRouter } from "./idea";
 
 export const appRouter = createRouter()
   .transformer(superjson)
+  .merge("idea.", ideaRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
