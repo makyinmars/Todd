6c0020c8f717cdeae590135a25551791fe2094a7
Franklin Jara <franklinjaradev@gmail.com>
2022-07-10 21:35:50 -0400

    :rocket: Set up
    Tailwindcss and adding router for idea

---
+++ src/server/router/idea.ts
@@ -0,0 +1,31 @@
+import { createRouter } from "./context";
+import { z } from "zod";
+import { Prisma } from "@prisma/client";
+import { prisma } from "@/server/db/client";
+import { TRPCError } from "@trpc/server/dist/declarations/src";
+
+const defaultIdeaSelect = Prisma.validator<Prisma.IdeaSelect>()({
+  id: true,
+  title: true,
+  content: true,
+  imageUrl: true,
+  votes: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export const ideaRouter = createRouter().query("getIdeas", {
+  async resolve() {
+    const ideas = await prisma.idea.findMany({
+      select: defaultIdeaSelect,
+    });
+    if (ideas) {
+      return ideas;
+    } else {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: `Could not get trips`,
+      });
+    }
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Ubuntu", ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
