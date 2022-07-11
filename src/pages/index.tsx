9511e37d1844f3a20656242f1200a485e91c8686
Franklin Jara <franklinjaradev@gmail.com>
2022-07-10 21:50:35 -0400

    :package: Update eslint and ts config
    Updating and fixing imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from "next";
-import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
   return (
     <div>
-      <h1 className="text-center">Todd Chavez's Crazy Ideas!!</h1>
+      <h1 className="text-center">Todd Chavez{"`"}s Crazy Ideas</h1>
     </div>
   );
 };
