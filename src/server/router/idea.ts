36203488ca42147cb0dff6072741101537f040b8
Franklin Jara <franklinjaradev@gmail.com>
2022-07-10 22:00:38 -0400

    :art: Fixing a bug
    Commenting out ideaSelect

--- src/server/router/idea.ts
+++ src/server/router/idea.ts
@@ -4,28 +4,29 @@ import { Prisma } from "@prisma/client";
 import { prisma } from "@/server/db/client";
 import { TRPCError } from "@trpc/server/dist/declarations/src";
 
-const defaultIdeaSelect = Prisma.validator<Prisma.IdeaSelect>()({
-  id: true,
-  title: true,
-  content: true,
-  imageUrl: true,
-  votes: true,
-  createdAt: true,
-  updatedAt: true,
-});
+// const defaultIdeaSelect = Prisma.validator<Prisma.IdeaSelect>()({
+//   id: true,
+//   title: true,
+//   content: true,
+//   imageUrl: true,
+//   votes: true,
+//   createdAt: true,
+//   updatedAt: true,
+// });
 
 export const ideaRouter = createRouter().query("getIdeas", {
   async resolve() {
-    const ideas = await prisma.idea.findMany({
-      select: defaultIdeaSelect,
-    });
-    if (ideas) {
-      return ideas;
-    } else {
-      throw new TRPCError({
-        code: "BAD_REQUEST",
-        message: `Could not get trips`,
-      });
-    }
+    // const ideas = await prisma.idea.findMany({
+    //   select: defaultIdeaSelect,
+    // });
+    // if (ideas) {
+    //   return ideas;
+    // } else {
+    //   throw new TRPCError({
+    //     code: "BAD_REQUEST",
+    //     message: `Could not get trips`,
+    //   });
+    // }
+    return null
   },
 });
